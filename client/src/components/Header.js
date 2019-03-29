1824dd5c52aa3f3b8cea220f6da9a3c55792a9c1
kkoomin <minha.koo.9@gmail.com>
2019-03-29 09:40:55 +0000

    heroku

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="main-header">
             <p className='header-name'>
-            <span style={{color: '#8d0422'}}>V</span>ino Note            
+            Vino Note            
             </p>
         </div>
     )
